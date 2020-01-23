3481ac58b7116a168fe19a16f5aefb0020eaab1a
Redouane <48132401+Rodoudou@users.noreply.github.com>
2020-01-23 15:08:56 +0100

    creation de la route offer/with-count

--- routes/annonce.js
+++ routes/annonce.js
@@ -14,47 +14,51 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 router.post("/offer/publish", isAuthenticated, async (req, res) => {
     const body = req.fields;
 
- try {
-     const newAnnonce = new Annonce({
-         title: body.title,
-         description: body.description,
-         price: body.price,
-         created: Date.now(),
-         creator:req.user
+    try {
+        const newAnnonce = new Annonce({
+            title: body.title,
+            description: body.description,
+            price: body.price,
+            created: Date.now(),
+            creator: req.user
         });
-//###############################################################
+        //###############################################################
 
         // faire en sorte que le titre, la description et le prix soit limités à :
         // description : 500 caractères
         // title : 50 caractères
         //  price : 100 000
-        console.log('body.title.length',body.title.length)
-if(body.title.length > 50 || body.description.length >500){
- return   res.json({error: 'vous avez depasser le nbr de caracteres'})
-} else if(body.price >100000){
-  return  res.json({error: 'Prix max est de 100 000'})
-}else{
-//###############################################################
-    
-    await newAnnonce.save();
-console.log(newAnnonce)
-    // on cherche tous les Annonces qui ont pour mail le mail reçu
+        console.log('body.title.length', body.title.length)
+        if (body.title.length > 50 || body.description.length > 500) {
+            return res.json({
+                error: 'vous avez depasser le nbr de caracteres'
+            })
+        } else if (body.price > 100000) {
+            return res.json({
+                error: 'Prix max est de 100 000'
+            })
+        } else {
+            //###############################################################
 
-    res.json({
-        _id:newAnnonce._id,
-        title: body.title,
-        description: body.description,
-        price: body.price,
-        created: Date.now(),
-        creator: {
-            account: {
-              username: req.user.account.username,
-              phone: req.user.account.phone
-            },
-            _id: req.user._id
-          }
-    });
-}
+            await newAnnonce.save();
+            console.log(newAnnonce)
+            // on cherche tous les Annonces qui ont pour mail le mail reçu
+
+            res.json({
+                _id: newAnnonce._id,
+                title: body.title,
+                description: body.description,
+                price: body.price,
+                created: Date.now(),
+                creator: {
+                    account: {
+                        username: req.user.account.username,
+                        phone: req.user.account.phone
+                    },
+                    _id: req.user._id
+                }
+            });
+        }
 
         // Ici renvoyer une reponse de la data de l'Annonce avec la reference de l'user
 
@@ -67,29 +71,58 @@ console.log(newAnnonce)
 });
 
 
-const createFilters= req =>{
-    const filters={},
+const createFilters = req => {
+    const filters = {},
 
-    if(req.query.priceMin){
-        filters.price={};
-        filters.price.$gte = req.query.priceMin;
-    }
-    if(req.query.priceMax){
-        if(filters.price === undefined){
-            filters.price={};
+        if (req.query.priceMin) {
+            filters.price = {};
+            filters.price.$gte = req.query.priceMin;
+        }
+    if (req.query.priceMax) {
+        if (filters.price === undefined) {
+            filters.price = {};
         }
         filters.price.$lte = req.query.priceMax;
     }
-    
-    if(req.query.price.title){
+
+    if (req.query.price.title) {
         filters.title = new RegExp(req.query.title, "i");
     }
     return filters;
 
 };
 
+// offer/with-count
+router.get("/offer/with-count", isAuthenticated, async (req, res) => {
+    const filters = createFilters(req);
 
+    // Ici, nous construisons notre recherche
+    const search = product.find(filters).populate("category");
 
+    if (req.query.sort === "price-desc") {
+        // Ici, nous continuons de construire notre recherche
+        search.sort({
+            price: 1
+        });
+    } else if (req.query.sort === "price-desc") {
+        // Ici, nous continuons de construire notre recherche
+        search.sort({
+            price: -1
+        });
+    }
+                    // limit : le nombre de résultats affichés
+                    // skip : Ignorer les X premiers
+    if(req.query.page){
+        const page = req.query.page;
+        const limit = 4;
+
+        search.limit(limit*(page-1));
+    }
+    // la recherche est déclenchée grâce au await
+    const products = await search;
+    res.json(products);
+
+});
 
 
 
